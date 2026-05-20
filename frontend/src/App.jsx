69d03e0eba4916ccac0c7b15768cc0a5ff23cde9
Ritik Mahawar <mahawarritik3@gmail.com>
2026-05-20 17:28:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     try {
 
       const response = await fetch(
-        "http://127.0.0.1:8000/analyze-resume/",
+        "https://resume-analyzer-1-7t4x.onrender.com/analyze-resume/",
         {
           method: "POST",
           body: formData,
